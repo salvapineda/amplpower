7445c082ca9e9cc42be7a4a5448dedfb67016666
Salvador Pineda Morente <salvapineda2@gmail.com>
2025-05-19 10:53:57 +0200

    0.0.26

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="amplpower",
-    version="0.0.25",
+    version="0.0.26",
     license="MIT",
     description="AMPL package for power systems",
     long_description="{}\n{}".format(
